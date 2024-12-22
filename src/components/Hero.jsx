07ca38dd92895073d78cfb75c90292985eb47c88
Anas raza <154615716+Muhammadanasraza@users.noreply.github.com>
2024-12-22 23:14:11 +0500

    add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,142 +1,83 @@
 import React from 'react'
 
-function Hero() {
-    return (
-        <section className="text-gray-600 body-font">
-  <div className="container max-w-screen-xl  px-5 py-24 mx-auto">
-  
-    <div className="flex flex-wrap -m-4">
-      <div className="lg:w-1/3 sm:w-1/2 p-4">
-        <div className="flex relative">
-          <img
-            alt="gallery"
-            className="absolute inset-0 w-full h-full object-cover object-center"
-            src="https://img.freepik.com/free-psd/back-school-new-normal-banner_23-2149027689.jpg?ga=GA1.1.518592586.1717923796&semt=ais_hybrid"
-          />
-          <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-              THE SUBTITLE
-            </h2>
-            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-              Shooting Stars
-            </h1>
-            <p className="leading-relaxed">
-              Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-              microdosing tousled waistcoat.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="lg:w-1/3 sm:w-1/2 p-4">
-        <div className="flex relative">
-          <img
-            alt="gallery"
-            className="absolute inset-0 w-full h-full object-cover object-center"
-            src="https://img.freepik.com/free-psd/online-courses-banner-template_23-2149109788.jpg?ga=GA1.1.518592586.1717923796&semt=ais_hybrid"
-          />
-          <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-              THE SUBTITLE
-            </h2>
-            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-              The Catalyzer
-            </h1>
-            <p className="leading-relaxed">
-              Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-              microdosing tousled waistcoat.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="lg:w-1/3 sm:w-1/2 p-4">
-        <div className="flex relative">
-          <img
-            alt="gallery"
-            className="absolute inset-0 w-full h-full object-cover object-center"
-            src="https://img.freepik.com/free-psd/children-school-education-youtube-cover_23-2149925217.jpg?ga=GA1.1.518592586.1717923796&semt=ais_hybrid"
-          />
-          <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-              THE SUBTITLE
-            </h2>
-            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-              The 400 Blows
-            </h1>
-            <p className="leading-relaxed">
-              Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-              microdosing tousled waistcoat.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="lg:w-1/3 sm:w-1/2 p-4">
-        <div className="flex relative">
-          <img
-            alt="gallery"
-            className="absolute inset-0 w-full h-full object-cover object-center"
-            src="https://img.freepik.com/free-vector/professional-business-tumblr-banner_742173-12161.jpg?ga=GA1.1.518592586.1717923796&semt=ais_hybrid"
-          />
-          <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-              THE SUBTITLE
-            </h2>
-            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-              Neptune
-            </h1>
-            <p className="leading-relaxed">
-              Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-              microdosing tousled waistcoat.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="lg:w-1/3 sm:w-1/2 p-4">
-        <div className="flex relative">
-          <img
-            alt="gallery"
-            className="absolute inset-0 w-full h-full object-cover object-center"
-            src="https://img.freepik.com/free-psd/e-learning-horizontal-banner-template-with-geometric-shapes_23-2149433275.jpg?ga=GA1.1.518592586.1717923796&semt=ais_hybrid"
-          />
-          <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-              THE SUBTITLE
-            </h2>
-            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-              Holden Caulfield
-            </h1>
-            <p className="leading-relaxed">
-              Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-              microdosing tousled waistcoat.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="lg:w-1/3 sm:w-1/2 p-4">
-        <div className="flex relative">
-          <img
-            alt="gallery"
-            className="absolute inset-0 w-full h-full object-cover object-center"
-            src="https://img.freepik.com/free-psd/back-school-banner-template-theme_23-2148633289.jpg?ga=GA1.1.518592586.1717923796&semt=ais_hybrid"
-          />
-          <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
-              THE SUBTITLE
-            </h2>
-            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-              Alper Kamu
-            </h1>
-            <p className="leading-relaxed">
-              Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-              microdosing tousled waistcoat.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+import { Card, CardHeader, CardTitle, CardContent } from './ui/card'
+import { Badge } from './ui/badge'
 
-    )
+function Hero() {
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container max-w-screen-xl  px-5 py-24 mx-auto">
+
+        <div className="flex flex-wrap -m-4">
+                        <div className="lg:w-1/3 sm:w-1/2 p-4">
+                            <div className="flex relative h-60 max-sm:w-[335px]">
+                                <img
+                                    alt="gallery"
+                                    className="absolute inset-0 w-full h-full object-cover rounded-xl object-center"
+                                    src="https://img.freepik.com/free-psd/back-school-new-normal-banner_23-2149027689.jpg?ga=GA1.1.518592586.1717923796&semt=ais_hybrid"
+                                />
+                                <Card className="p-2 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100 transition-opacity duration-300">
+                                    <CardHeader>
+                                        <CardTitle className="text-2xl font-bold">
+                                            Web and Mobile Development
+                                        </CardTitle>
+                                    </CardHeader>
+                                    <CardContent className="space-y-4">
+                                        <div className="grid grid-col gap-4">
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio magnam.</p>
+                                        </div>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                        </div>
+                        <div className="lg:w-1/3 sm:w-1/2 p-4">
+                            <div className="flex relative h-60 max-sm:w-[335px]">
+                                <img
+                                    alt="gallery"
+                                    className="absolute inset-0 w-full h-full object-cover rounded-xl object-center"
+                                    src="https://img.freepik.com/free-psd/back-school-new-normal-banner_23-2149027689.jpg?ga=GA1.1.518592586.1717923796&semt=ais_hybrid"
+                                />
+                                <Card className="p-2 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100 transition-opacity duration-300">
+                                    <CardHeader>
+                                        <CardTitle className="text-2xl font-bold">
+                                            Web and Mobile Development
+                                        </CardTitle>
+                                    </CardHeader>
+                                    <CardContent className="space-y-4">
+                                        <div className="grid grid-col gap-4">
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio magnam.</p>
+                                        </div>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                        </div>
+                        <div className="lg:w-1/3 sm:w-1/2 p-4">
+                            <div className="flex relative h-60 max-sm:w-[335px]">
+                                <img
+                                    alt="gallery"
+                                    className="absolute inset-0 w-full h-full object-cover rounded-xl object-center"
+                                    src="https://img.freepik.com/free-psd/back-school-new-normal-banner_23-2149027689.jpg?ga=GA1.1.518592586.1717923796&semt=ais_hybrid"
+                                />
+                                <Card className="p-2 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100 transition-opacity duration-300">
+                                    <CardHeader>
+                                        <CardTitle className="text-2xl font-bold">
+                                            Web and Mobile Development
+                                        </CardTitle>
+                                    </CardHeader>
+                                    <CardContent className="space-y-4">
+                                        <div className="grid grid-col gap-4">
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio magnam.</p>
+                                        </div>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                        </div>
+        
+                    </div>
+      </div>
+    </section >
+
+  )
 }
 
 export default Hero
