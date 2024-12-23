059f9765a9b1bb3a1c86c8830477806cd40fe22d
Anas raza <154615716+Muhammadanasraza@users.noreply.github.com>
2024-12-23 19:04:48 +0500

    add

--- src/components/Trainer.jsx
+++ src/components/Trainer.jsx
@@ -8,9 +8,11 @@ export default function Trainer() {
     return (
         <div className="">
             {/* max-w-screen-xl mx-auto */}
-            <h1 className="flex justify-center font-bold p-10 text-3xl underline">Student Info:</h1>
+            <h1 className="title-font text-center sm:text-4xl text-3xl py-10 mb-4 font-bold text-gray-900">
+               Students List
+            </h1>
             <div className="p-5 ">
-                <StudentTable columns={columns} data={studentData}  />
+                <StudentTable columns={columns} data={studentData} />
             </div>
             {/* <Button className="max-sm ">Login </Button> */}
         </div>
